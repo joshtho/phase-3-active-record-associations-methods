982e474398ea0ba1ce9983dca9d9360a64b52f9b
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-11-16 12:17:23 -0600

    commit

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,16 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.sum(:genre_id)
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.songs.sum(:artist_id)
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    
   end
 end
